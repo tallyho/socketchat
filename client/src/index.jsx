43e42b765b41771d07e62a93ef9e4ab25063355f
Jesse Tannahill <jesset@austinsigma.com>
2016-05-05 19:34:30 -0500

    Client with react, webpack, and hot reloading

---
+++ client/src/index.jsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+class App extends React.Component {
+  render() {return (
+    <div>Hell3, World!!!!</div>
+  )}
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
