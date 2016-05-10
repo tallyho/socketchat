3a438af583498dbde380b0d023f06242a2e88074
Jesse Tannahill <jesset@austinsigma.com>
2016-05-10 00:22:59 -0500

    client: fix async and add socket.io connection

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,11 +2,15 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import {createStore, combineReducers} from 'redux'
 import {Provider} from 'react-redux'
-import {user} from './user'
-import App from './components/App'
 import {Map} from 'immutable'
 
+import {user} from './user'
+import App from './components/App'
+import setupSocket from './socket'
+
 const store = createStore(combineReducers({user}))
+
+setupSocket(store)
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ client/src/socket.js
@@ -0,0 +1,37 @@
+import io from 'socket.io-client'
+
+var socket = null
+
+const connectSocket = () => {
+  socket = io.connect('http://localhost:3000')
+
+  const states = [
+    'connect',
+    'connect_error',
+    'connect_timeout',
+    'reconnect',
+    'reconnecting',
+    'reconnect_error',
+    'reconnect_failed'
+  ]
+  for (let state of states) {
+    socket.on(state, () => {
+      console.log('socket:', socket.connected)
+    })
+  }
+}
+
+export default (store) => {
+  store.subscribe(() => {
+    const state = store.getState()
+    if (socket === null && state.user.get('token') != null) {
+      console.log("connecting socket")
+      connectSocket()
+    } else if (socket !== null && state.user.get('token') == null) {
+      console.log("disconnecting socket")
+      socket.disconnect()
+      socket = null
+    }
+  })
+}
+
